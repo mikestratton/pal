6c067f1885928391aea7dc92ff02631222385d01
mikestratton <mike@mikestratton.net>
2019-04-22 13:17:34 -0400

    logo and laravelcollective

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+{{--                    {{ config('app.name', 'PAL') }}--}}
+                    {{ Html::image("img/pal_logo.svg", "PAL Logo",  array( 'width' => 'auto', 'height' => 40 )) }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
