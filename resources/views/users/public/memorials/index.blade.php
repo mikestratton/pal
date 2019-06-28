e2a0a2138af127469f4d163e994c88e135fc934c
mikestratton <mike@mikestratton.net>
2019-06-28 17:15:54 -0400

    css

--- resources/views/users/public/memorials/index.blade.php
+++ resources/views/users/public/memorials/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container">
+<div class="container bg-light p-5 border border-primary rounded mb-5">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <h1>
